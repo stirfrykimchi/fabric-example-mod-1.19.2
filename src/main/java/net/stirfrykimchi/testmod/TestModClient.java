7f02fa14a7a57140eb2853954b0c89a1ab41404b
stirfrykimchi <donkeygottapoo@gmail.com>
2023-03-31 14:11:39 -1000

    Initial commit

---
+++ src/main/java/net/stirfrykimchi/testmod/TestModClient.java
@@ -0,0 +1,11 @@
+package net.stirfrykimchi.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Very important comment hehe :)
+
+    }
+}
